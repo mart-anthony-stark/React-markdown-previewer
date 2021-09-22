a0ee10f8559ca0c3beaa1d7252e6707004799c19
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-22 17:33:02 +0800

    1 file modified

--- src/Preview.js
+++ src/Preview.js
@@ -2,10 +2,18 @@ import React from 'react';
 import marked from 'marked';
 
 export default function Preview({ markdown }) {
+  // ALLOWS LINE BREAKS WITH RETURN BUTTON
+  marked.setOptions({
+    breaks: true,
+    highlight: function (code) {
+      return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+    },
+  });
   const renderer = new marked.Renderer();
   renderer.link = function (href, title, text) {
     return `<a target="_blank" href="${href}">${text}</a>`;
   };
+
   return (
     <div
       dangerouslySetInnerHTML={{ __html: marked(markdown, { renderer }) }}
