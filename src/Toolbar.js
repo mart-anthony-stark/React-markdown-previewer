bfe35ccf5fb02aac95f2244bbc53747a53cb115e
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-22 20:50:14 +0800

    3 files modified

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -4,6 +4,9 @@ export default function ({ title }) {
   return (
     <div className="toolbar">
       <span className="title">{title}</span>
+      <div className="maximize">
+        <i class="fa fa-arrows-alt" aria-hidden="true"></i>
+      </div>
     </div>
   );
 }
